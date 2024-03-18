5a4d9e0c0977b74e2f0421c5ede78ebf1b119bcd
evokerking1 <evokerking1@users.noreply.github.com>
2024-03-18 12:56:38 +0000

    Apply automatic changes

---
+++ out/IDK 1.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*********************!*\
+  !*** ./src/Main.ts ***!
+  \*********************/
+ModAPI.displayToChat({ msg: "hello, this is eaglerforge!" });
+ModAPI.logger.loginfo({ string: "you can log a string to console" });
+ModAPI.uwuify({ string: "and even uwuify a string?" });
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiSURLIDEuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSx1QkFBdUIsb0NBQW9DO0FBQzNELHdCQUF3QiwyQ0FBMkM7QUFDbkUsZ0JBQWdCLHFDQUFxQyIsInNvdXJjZXMiOlsid2VicGFjazovL0lESyAxLy4vc3JjL01haW4udHMiXSwic291cmNlc0NvbnRlbnQiOlsiTW9kQVBJLmRpc3BsYXlUb0NoYXQoeyBtc2c6IFwiaGVsbG8sIHRoaXMgaXMgZWFnbGVyZm9yZ2UhXCIgfSk7XG5Nb2RBUEkubG9nZ2VyLmxvZ2luZm8oeyBzdHJpbmc6IFwieW91IGNhbiBsb2cgYSBzdHJpbmcgdG8gY29uc29sZVwiIH0pO1xuTW9kQVBJLnV3dWlmeSh7IHN0cmluZzogXCJhbmQgZXZlbiB1d3VpZnkgYSBzdHJpbmc/XCIgfSk7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
