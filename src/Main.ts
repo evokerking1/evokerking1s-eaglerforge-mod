a30e194a98b3c3294fd6ed8d0b6ca52bafb794cb
evokerking1 <118303818+evokerking1@users.noreply.github.com>
2024-03-18 16:24:48 +0000

    update

--- src/Main.ts
+++ src/Main.ts
@@ -1,9 +1,8 @@
 import { require_all } from "./util/burmod/require_all"
+require_all()
 import { handle } from "./util/burmod/utils/commandHandler"
 
-
-require_all()
 ModAPI.displayToChat({msg: "hello, this is eaglerforge!"})
 ModAPI.logger.loginfo({string: "you can log a string to console"})
 ModAPI.uwuify({string: "and even uwuify a string?"})
-handle()
+
