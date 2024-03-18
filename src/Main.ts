749a27a0f7817c2896af88eb5f403c59ed24ba72
evokerking1 <118303818+evokerking1@users.noreply.github.com>
2024-03-18 19:27:45 +0000

    f

--- src/Main.ts
+++ src/Main.ts
@@ -10,3 +10,4 @@ ModAPI.displayToChat({msg: "hello, this is eaglerforge!"})
 ModAPI.logger.loginfo({string: "you can log a string to console"})
 ModAPI.uwuify({string: "and even uwuify a string?"})
 
+ModAPI.
