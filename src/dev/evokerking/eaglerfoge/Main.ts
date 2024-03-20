ced9d67a1affdccf10321a5d9cba036c347e4220
evokerking1 <118303818+evokerking1@users.noreply.github.com>
2024-03-20 16:55:03 +0000

    Yet another update

--- src/dev/evokerking/eaglerfoge/Main.ts
+++ src/dev/evokerking/eaglerfoge/Main.ts
@@ -1,8 +1,7 @@
 import { require_all } from "../../../io/github/murturtle/burmod/require_all"
 require_all()
-import { handle } from "../../../io/github/murturtle/burmod/utils/commandHandler"
-import { logToConsole } from "../../../io/github/murturtle/burmod/utils/consoleLogger"
-import "./util/mc"
+import { handle } from "../../../io/github/murturtle/burmod/util/commandHandler"
+import { logToConsole } from "../../../io/github/murturtle/burmod/util/consoleLogger"
 //logToConsole(msg:"Starting.");
 //logToConsole(msg:,"Starting..");
 //logToConsole(msg:"Starting...");
--- src/dev/evokerking/eaglerfoge/versioncommand.ts
+++ src/dev/evokerking/eaglerfoge/versioncommand.ts
@@ -1,7 +1,7 @@
 import { require_all } from "../../../io/github/murturtle/burmod/require_all"
-import { logToChat } from "../../../io/github/murturtle/burmod/utils/chatLogger"
-import { logToConsole } from "../../../io/github/murturtle/burmod/utils/consoleLogger"
-import { handle } from "../../../io/github/murturtle/burmod/utils/commandHandler"
+import { logToChat } from "../../../io/github/murturtle/burmod/util/chatLogger"
+import { logToConsole } from "../../../io/github/murturtle/burmod/util/consoleLogger"
+import { handle } from "../../../io/github/murturtle/burmod/util/commandHandler"
 require_all()
 //handle(msg:"version")
 
